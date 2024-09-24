3504d4adb706825daae9a523df0e62ef7800da4b
gorvinet <gorvinet@gmail.com>
2024-09-24 14:10:24 +0200

    first commit

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\Work\\Libraries\\flutter\\flutter_windows_3.19.1-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\Work\\Projects\\sea_battle" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\Work\\Libraries\\flutter\\flutter_windows_3.19.1-stable\\flutter"
+  "PROJECT_DIR=D:\\Work\\Projects\\sea_battle"
+  "FLUTTER_ROOT=D:\\Work\\Libraries\\flutter\\flutter_windows_3.19.1-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\Work\\Projects\\sea_battle\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\Work\\Projects\\sea_battle"
+  "FLUTTER_TARGET=D:\\Work\\Projects\\sea_battle\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC8yMzVkYjkxMWJhMjc5NzIyZjVlNjg1ZjM4YjBlZDMwZmE3ZTg1NzBhLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\Work\\Projects\\sea_battle\\.dart_tool\\package_config.json"
+)
